49cf30ea76126b753ecc7348ab681488a70ca165
zalimannard <zalimannard@mail.ru>
2022-03-15 01:59:55 +0300

    Dynamic time change

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include "AsciiStorage.h"
 #include "AsciiChar.h"
 #include "AsciiString.h"
+#include <unistd.h>
+#include <cstdlib>
 #include <ctime>
 #include <fstream>
 #include <iostream>
@@ -12,7 +14,7 @@
 int main()
 {
     AsciiStorage storage(13, 7);
-    std::ifstream fin("../numbers");
+    std::ifstream fin("pctime.txt");
     while (!fin.eof())
     {
         AsciiChar tmp(13, 7);
@@ -20,18 +22,54 @@ int main()
         storage.set(tmp.getNonAsciiSymbol(), tmp);
     }
 
-    time_t t = time(NULL);
-    struct tm tm = *localtime(&t);
-
-    std::string phraseToPrint = std::to_string(tm.tm_hour) + ":" + std::to_string(tm.tm_min);
-    AsciiString asciiString;
-    asciiString.setOutputShift(4);
-
-    for (auto ch : phraseToPrint)
+    bool isDotsVisible = true;
+    while (true)
     {
-        asciiString += storage.get(ch);
+        system("clear");
+        time_t t = time(NULL);
+        struct tm tm = *localtime(&t);
+    
+        std::string hours = std::to_string(tm.tm_hour);
+        if (hours.length() == 1)
+        {  
+            hours = "0" + hours;
+        }
+        std::string minutes = std::to_string(tm.tm_min);
+        if (minutes.length() == 1)
+        {
+            minutes = "0" + minutes;
+        }
+
+        std::string phraseToPrint;
+
+        if (isDotsVisible)
+        {
+            phraseToPrint = hours + ":" + minutes;
+        }
+        else
+        {
+            phraseToPrint = hours + "_" + minutes;
+        }
+        AsciiString asciiString;
+        asciiString.setOutputShift(4);
+
+        for (auto ch : phraseToPrint)
+        {
+            asciiString += storage.get(ch);
+        }
+        std::cout << asciiString << std::endl;
+
+        if (isDotsVisible)
+        {
+            usleep(500 * 1000);
+        }
+        else
+        {
+            usleep(500 * 1000);
+        }
+
+        isDotsVisible = !isDotsVisible;
     }
-    std::cout << asciiString << std::endl;
 
     return 0;
 }
